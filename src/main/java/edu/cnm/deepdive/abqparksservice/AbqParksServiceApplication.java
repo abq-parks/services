9e1ef87f6dd7dd7f96e81dcc957e66f5101161c7
Michael Lacey <laceymwes@gmail.com>
2018-06-29 10:44:44 -0600

    -Initial commit.

---
+++ src/main/java/edu/cnm/deepdive/abqparksservice/AbqParksServiceApplication.java
@@ -0,0 +1,12 @@
+package edu.cnm.deepdive.abqparksservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbqParksServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AbqParksServiceApplication.class, args);
+  }
+}
