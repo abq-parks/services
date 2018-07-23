c5a8e6b3db81f47fc0f47e5b9589a57575fb53df
Jason Rafferty <Jason0x55@gmail.com>
2018-07-23 15:24:38 -0600

    Amenity class updated.

--- src/main/java/edu/cnm/deepdive/abqparksservice/model/entity/Amenity.java
+++ src/main/java/edu/cnm/deepdive/abqparksservice/model/entity/Amenity.java
@@ -1,19 +1,25 @@
 package edu.cnm.deepdive.abqparksservice.model.entity;
 
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Amenity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "amenity_id", nullable = false, updatable = false)
   private long id;
 
   @Column(nullable = false)
   private String name;
+
+  @OneToMany(mappedBy = "Amenity")
+  private List<ParkAmenity> parkAmenities;
 
 }
