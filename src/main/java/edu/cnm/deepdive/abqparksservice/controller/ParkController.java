80e661767a813062bdcb1a1d921f078d2af2dd66
Michael Lacey <laceymwes@gmail.com>
2018-07-24 10:55:24 -0600

    - Implement NoSuchElementException in ParkController.

--- src/main/java/edu/cnm/deepdive/abqparksservice/controller/ParkController.java
+++ src/main/java/edu/cnm/deepdive/abqparksservice/controller/ParkController.java
@@ -3,12 +3,16 @@ package edu.cnm.deepdive.abqparksservice.controller;
 import edu.cnm.deepdive.abqparksservice.model.dao.ParkRepository;
 import edu.cnm.deepdive.abqparksservice.model.entity.Amenity;
 import edu.cnm.deepdive.abqparksservice.model.entity.Park;
+import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.ExposesResourceFor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,16 +26,20 @@ public class ParkController {
   public ParkController(ParkRepository parkRespository) {
     this.parkRepository = parkRespository;
   }
-  
 
   @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
   public Iterable<Park> allParks() {
-    return parkRepository.findAl();
+    return parkRepository.findAll();
   }
 
   @GetMapping(value = "/{amenities}", produces = MediaType.APPLICATION_JSON_VALUE)
   public Iterable<Park> searchParks(@PathVariable("amenities")Amenity... amenities) {
     return parkRepository.findAllByParkAmenities(amenities);
+  }
+
+  @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Resource not found")
+  @ExceptionHandler(NoSuchElementException.class)
+  public void notFound() {
   }
 
 }
