863ee3886ecbedece2c7f9503be302329770e901
Jason Rafferty <Jason0x55@gmail.com>
2018-07-24 10:03:49 -0600

    Amenity class updated.

--- src/main/java/edu/cnm/deepdive/abqparksservice/model/entity/Amenity.java
+++ src/main/java/edu/cnm/deepdive/abqparksservice/model/entity/Amenity.java
@@ -1,15 +1,30 @@
 package edu.cnm.deepdive.abqparksservice.model.entity;
 
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityLinks;
 
 @Entity
 public class Amenity {
+
+  private static EntityLinks entityLinks;
+
+  @PostConstruct
+  private void init() {
+    String ignore = entityLinks.toString();
+  }
+
+  @Autowired
+  private void setEntityLinks(EntityLinks entityLinks) {
+    Amenity.entityLinks = entityLinks;
+  }
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
@@ -22,4 +37,24 @@ public class Amenity {
   @OneToMany(mappedBy = "Amenity")
   private List<ParkAmenity> parkAmenities;
 
+  public long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public List<ParkAmenity> getParkAmenities() {
+    return parkAmenities;
+  }
+
+  public void setParkAmenities(
+      List<ParkAmenity> parkAmenities) {
+    this.parkAmenities = parkAmenities;
+  }
 }
