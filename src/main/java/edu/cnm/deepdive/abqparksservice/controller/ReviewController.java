03433b0c5407248207608b6a99f317ca4766c5d5
Michael Lacey <laceymwes@gmail.com>
2018-07-24 11:04:40 -0600

    - Implement NoSuchElementException in ReviewController.

--- src/main/java/edu/cnm/deepdive/abqparksservice/controller/ReviewController.java
+++ src/main/java/edu/cnm/deepdive/abqparksservice/controller/ReviewController.java
@@ -2,12 +2,16 @@ package edu.cnm.deepdive.abqparksservice.controller;
 
 import edu.cnm.deepdive.abqparksservice.model.dao.ReviewRepository;
 import edu.cnm.deepdive.abqparksservice.model.entity.Review;
+import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.ExposesResourceFor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,5 +30,9 @@ public class ReviewController {
   public Iterable<Review> getReviews(@PathVariable("parkId") long parkId) {
     return reviewRepository.findAllByParkOrderByReviewDesc(parkId);
   }
-
+  
+  @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Resource not found")
+  @ExceptionHandler(NoSuchElementException.class)
+  public void notFound() {
+  }
 }
