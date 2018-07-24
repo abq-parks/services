a88f0e21c51cfaf329eecb228f30b31b683a150a
Michael Lacey <laceymwes@gmail.com>
2018-07-24 10:19:41 -0600

    - Create ParkController.

---
+++ src/main/java/edu/cnm/deepdive/abqparksservice/controller/ParkController.java
@@ -0,0 +1,30 @@
+package edu.cnm.deepdive.abqparksservice.controller;
+
+import edu.cnm.deepdive.abqparksservice.model.dao.ParkRepository;
+import edu.cnm.deepdive.abqparksservice.model.entity.Amenity;
+import edu.cnm.deepdive.abqparksservice.model.entity.Park;
+import org.springframework.hateoas.ExposesResourceFor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ExposesResourceFor(Park.class)
+@RequestMapping("/parks/")
+public class ParkController {
+
+  private ParkRepository parkRepository;
+
+  @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Iterable<Park> allParks() {
+    return parkRepository.findAl();
+  }
+
+  @GetMapping(value = "/{amenities}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Iterable<Park> searchParks(@PathVariable("amenities")Amenity... amenities) {
+    return parkRepository.findAllByParkAmenities(amenities);
+  }
+
+}
