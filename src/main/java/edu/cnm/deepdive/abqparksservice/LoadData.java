ff7c9ae00b6905c1436d2c74dca6752ef6bc1e60
Jason Rafferty <Jason0x55@gmail.com>
2018-08-03 13:24:24 -0600

    Added default users when database is populated with parks.

--- src/main/java/edu/cnm/deepdive/abqparksservice/LoadData.java
+++ src/main/java/edu/cnm/deepdive/abqparksservice/LoadData.java
@@ -2,6 +2,8 @@ package edu.cnm.deepdive.abqparksservice;
 
 import edu.cnm.deepdive.abqparksservice.model.dao.AmenityRepository;
 import edu.cnm.deepdive.abqparksservice.model.dao.ParkRepository;
+import edu.cnm.deepdive.abqparksservice.model.dao.UserRepository;
+import edu.cnm.deepdive.abqparksservice.model.entity.User;
 import edu.cnm.deepdive.abqparksservice.services.LoadParks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -13,12 +15,14 @@ public class LoadData implements ApplicationRunner {
 
   private AmenityRepository amenityRepository;
   private ParkRepository parkRepository;
+  private UserRepository userRepository;
 
   @Autowired
   public LoadData(AmenityRepository amenityRepository,
-      ParkRepository parkRepository) {
+      ParkRepository parkRepository, UserRepository userRepository) {
     this.amenityRepository = amenityRepository;
     this.parkRepository = parkRepository;
+    this.userRepository = userRepository;
   }
 
   @Override
@@ -26,6 +30,16 @@ public class LoadData implements ApplicationRunner {
     if (parkRepository.findAll().size() == 0) {
       LoadParks loadParks = new LoadParks(amenityRepository, parkRepository);
       loadParks.readJson();
+      User defaultUser = new User();
+      defaultUser.setFirstName("Default");
+      defaultUser.setLastName("User");
+      defaultUser.setUserEmail("Default@email.com");
+      userRepository.save(defaultUser);
+      User defaultUser2 = new User();
+      defaultUser2.setFirstName("Default");
+      defaultUser2.setLastName("User 2");
+      defaultUser2.setUserEmail("Default2@email.com");
+      userRepository.save(defaultUser2);
     }
   }
 }
